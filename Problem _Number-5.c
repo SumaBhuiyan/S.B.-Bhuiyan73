8190ba6ba078257c7055affd62e2181b05bba18c
SumaBhuiyan <sumabhuiyan127373@gmail.com>
2024-05-12 18:10:58 +0600

    Update

---
+++ Problem _Number-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float sum = 0.0, average;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    float arr[n];
+
+    printf("Enter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%f", &arr[i]);
+        sum += arr[i]; 
+    }
+
+    average = sum / n;
+    
+    printf("Sum of the array elements is: %.2f\n", sum);
+    printf("Average of the array elements is: %.2f\n", average);
+
+    return 0;
+}
